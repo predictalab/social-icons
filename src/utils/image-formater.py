97e2d7750c2861df37c70fb826a883214662fd98
Nicolas <nicolas@predictalab.com>
2025-04-16 12:07:59 +0200

    Improves image processing efficiency
    Adds a check to avoid processing images smaller
    than or equal to 150px in both dimensions,
    enhancing efficiency by skipping unnecessary
    resizing operations.

--- src/utils/image-formater.py
+++ src/utils/image-formater.py
@@ -18,7 +18,12 @@ allowed_extensions = ('.jpg', '.jpeg', '.png', '.webp')
 def process_image(file_path, output_path):
     try:
         with Image.open(file_path) as img:
-            # Conserver le ratio en redimensionnant pour que ni la largeur ni la hauteur ne dépasse 150 pixels
+            # Ne traiter que les images dont l'une des dimensions est supérieure à 150 pixels
+            if img.width <= 150 and img.height <= 150:
+                print(f"Image ignorée (taille inférieure ou égale à 150px) : {file_path}")
+                return
+            
+            # Redimensionner en conservant le ratio pour que ni la largeur ni la hauteur ne dépasse 150 pixels
             img.thumbnail((150, 150), Image.LANCZOS)
             
             ext = os.path.splitext(file_path)[1].lower()
